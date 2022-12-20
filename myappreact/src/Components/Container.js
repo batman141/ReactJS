f31f13f24ac2f51a8e4ade4ae755747098b02617
Utkarsh Raj <iminvincibleut@gmail.com>
2022-12-21 01:09:37 +0530

    Add container to list

--- myappreact/src/Components/Container.js
+++ myappreact/src/Components/Container.js
@@ -1,7 +1,21 @@
 import FlexComp from "./FlexComp";
 import "./Container.css";
+import { useState } from "react";
 
 function Container() {
+  const [backgroundClr, setBackgroundColor] = useState("white");
+  const colorChangeHandler = (color) => {
+    setBackgroundColor(
+      color
+      // "rgb(" +
+      //   [
+      //     Math.floor(Math.random() * 256),
+      //     Math.floor(Math.random() * 256),
+      //     Math.floor(Math.random() * 256),
+      //   ].join(",") +
+      //   ")"
+    );
+  };
   const data = [
     { id: 1, name: "Multi-Page Responsive Website" },
     { id: 2, name: "Existing Website Template" },
@@ -9,9 +23,19 @@ function Container() {
     { id: 4, name: "Small Website" },
   ];
 
-  const listItems = data.map((d) => <FlexComp id={d.id} message={d.name} />);
+  const listItems = data.map((d) => (
+    <FlexComp
+      id={d.id}
+      message={d.name}
+      colorsChangeHandler={colorChangeHandler}
+    />
+  ));
 
-  return <div className="element">{listItems}</div>;
+  return (
+    <div className="element" style={{ backgroundColor: backgroundClr }}>
+      {listItems}
+    </div>
+  );
 }
 
 export default Container;
--- myappreact/src/Components/FlexComp.js
+++ myappreact/src/Components/FlexComp.js
@@ -1,12 +1,40 @@
 import logo from "./logo1.svg";
 import "./FlexComp.css";
+import { useState } from "react";
 
 function FlexComp(props) {
+  const [value, setValue] = useState(100);
+  const [divValue, setDivValue] = useState({
+    yellowValue: props.id,
+    blueValue: props.message,
+  });
+
+  const yellowHandler = () => {
+    const newColor = Math.floor(Math.random() * 101);
+    props.colorsChangeHandler(newColor >= 50 ? "green" : "red");
+    setDivValue((prev) => {
+      return { ...prev, yellowValue: newColor };
+    });
+  };
+
+  const blueHandler = () => {
+    setDivValue((prev) => {
+      return { ...prev, blueValue: Math.floor(Math.random() * 100) };
+    });
+  };
   return (
-    <div className="container">
-      <div className="num">{props.id}</div>
-      <div className="container2">
-        <div className="desc">{props.message}</div>
+    <div
+      className="container"
+      onClick={() => {
+        setValue(200);
+        console.log("i am clicked");
+      }}
+    >
+      <div className="num" onClick={yellowHandler}>
+        {divValue.yellowValue}
+      </div>
+      <div className="container2" onClick={blueHandler}>
+        <div className="desc">{divValue.blueValue}</div>
         <img src={logo} className="icon" alt="logo" />
       </div>
     </div>
